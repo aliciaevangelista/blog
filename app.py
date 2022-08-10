2f27da27a62b781f7f311ec0fd648bd6b9643bad
Alícia Evangelista <itsaliduarte@gmail.com>
2022-08-10 12:56:00 +0000

    Nova rota contatos criada.

--- app.py
+++ app.py
@@ -4,4 +4,8 @@ app = Flask("hello")
 
 @app.route("/")
 def hello():
-    return "Hello World"git ass .
+    return "Hello World"
+
+@app.route("/contatos")
+def contato():
+    return "Alícia Duarte Evangelista"
